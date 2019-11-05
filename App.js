3e910ad63323a8d4fc5266a388cdcb660c2ad37b
imimali <imre.mali@sectorlabs.ro>
2019-11-05 11:02:56 +0200

    now it looks pretty

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
 
 import Router from './app/config/routes'
+import {fontFamily} from "./app/styles/theme";
+import {moderateScale as normalize} from 'react-native-size-matters';
 
 export default class App extends React.Component {
-  render() {
-    return (
-        /*<Provider store={store}>*/
-        <Router />
-        // </Provider>
-    );
-  }
+    render() {
+        return (
+            /*<Provider store={store}>*/
+            <View style={styles.container}>
+                <Text style={styles.textBox}>Exorcizamus te omnis imundus spiritus omnis satanica potestas omnis
+                    incursio infernalis adversarii</Text>
+            </View>
+            // </Provider>?
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#75b',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+
+    },
+    textBox: {
+        fontSize: normalize(36),
+        margin: normalize(20)
+    }
 });
