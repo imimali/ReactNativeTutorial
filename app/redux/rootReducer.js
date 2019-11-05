b8ba70fb05f79f0202c2ca93975fc81e8466ea3d
imimali <imre.mali@sectorlabs.ro>
2019-11-05 11:23:19 +0200

    add actions and reducers

--- app/redux/rootReducer.js
+++ app/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import homeReducer from "../modules/home/reducer";
 
 // Combine all the reducers
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({ homeReducer});
 
 export default rootReducer;
